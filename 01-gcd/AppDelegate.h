ecfadfea3f15dc6e66570a86c1763e0159361ee1
poioper <xfzhou1990@163.com>
2016-02-02 16:53:15 -0500

    Initial Commit

---
+++ 01-gcd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-gcd
+//
+//  Created by apple on 16/2/2.
+//  Copyright © 2016年 xiaofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-gcd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01-gcd
+//
+//  Created by apple on 16/2/2.
+//  Copyright © 2016年 xiaofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
